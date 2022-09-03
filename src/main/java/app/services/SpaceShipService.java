c0e37c0c6e684d5acba1436c5cd5178195dbd54a
kimbi91 <subscribefan@gmail.com>
2022-09-03 09:41:36 +0200

    task 1 done

---
+++ src/main/java/app/services/SpaceShipService.java
@@ -0,0 +1,23 @@
+package app.services;
+
+import app.models.SpaceShip;
+import app.repositories.SpaceShipRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
+
+@Service
+public class SpaceShipService {
+
+    private final SpaceShipRepo spaceShipRepo;
+
+    public SpaceShipService(SpaceShipRepo spaceShipRepo) {
+        this.spaceShipRepo = spaceShipRepo;
+    }
+
+    public List<SpaceShip> getAll() {
+        return new ArrayList<>((Collection) spaceShipRepo.findAll());
+    }
+}
